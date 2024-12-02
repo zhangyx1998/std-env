982a7c70c1e4a7ff9286920445c9968814c835e1
Yuxuan Zhang <github@z-yx.cc>
2024-12-02 18:55:33 -0500

    fix syntax error in setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ LOCAL="/usr/local/std-env"
 echo "Setting up STD-ENV for ${OS}..."
 
 # Test read and write access to HOME
-if [ ! -d ${HOME} || ! -r ${HOME} || ! -w ${HOME} ]; then
+if [ ! -d ${HOME} ] || [ ! -r ${HOME} ] || [ ! -w ${HOME} ]; then
   echo "Error: ${HOME} is not accessible."
   exit 1
 fi
@@ -16,11 +16,11 @@ fi
 if [ ! -d /usr/local/std-env ]; then
   echo "Cloning from ${REMOTE} into ${LOCAL}..." \
   && sudo git clone ${REMOTE} ${LOCAL} \
-  && cd ${LOCAL} || exit 1 \
+  && cd ${LOCAL} \
   && make install
 else
   echo "Updating from ${REMOTE}..." \
   && cd ${LOCAL} \
-  && git pull origin master
+  && git pull origin master \
   && make install
 fi
